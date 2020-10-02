3430a1cbabd991cecc6273bc75fb3fd0378aa226
Ejiroghene Obiuwevbi <ejiroghene15@gmail.com>
2020-10-02 03:25:58 +0100

    update

---
+++ src/app/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { map } from "rxjs/operator/map";
+
+@Injectable()
+export class ProductService {
+  private _albumUrl = "../assets/album.json";
+  constructor(private _http: Http) {}
+  getAlbum(id: number): any {
+    this._http
+      .get(this._albumUrl)
+      .forEach((response: Response) => response.json());
+  }
+}
